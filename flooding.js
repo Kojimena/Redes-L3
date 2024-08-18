dfb98da68a11b19a0a325a48b982c31283183c2a
Jimena Hernández <her21199@uvg.edu.gt>
2024-08-17 22:53:26 -0600

    flooding
    Co-Authored-By: Mark Albrand <mark.albrand@protonmail.com>

--- flooding.js
+++ flooding.js
@@ -36,26 +36,6 @@ xmpp.on("offline", () => {
   xmpp.stop();
 });
 
-xmpp.on("stanza", async (stanza) => {
-  if (stanza.is("message")) {
-    await xmpp.send(xml("presence", { type: "unavailable" }));
-    await xmpp.stop();
-  }
-});
-
-const flooding = (to, body) => {
-  if (contacts.includes(to)) {
-      console.log(`Message: ${body} sent to ${to}`);
-      sendMessage(to, body);
-  } else {
-      contacts.forEach((contact) => {
-          console.log(`Message: ${body} sent to ${contact}`);
-          sendMessage(contact, body);
-      });
-  }
-  
-}
-
 xmpp.on("online", async () => {
   console.log("---online");
   await xmpp.send(xml("presence"));
@@ -72,11 +52,73 @@ async function connect() {
     await xmpp.start();
 }
 
+/**
+ * Recibe un mensaje y si no es para nosotros, lo reenvía a todos los contactos
+ * @param {any} "stanza"
+ * @param {any} 
+ * @returns {any}
+ */
+xmpp.on("stanza", async (stanza) => {
+  if (stanza.is('message') && stanza.getChild('body')) {
+    const from = stanza.attrs.from;
+    const body = stanza.getChildText('body');
+    
+    console.log(`Received message from ${from}: ${body}`);
+    msg = parseMessage(body);
+
+    console.log("Username: ", xmpp.jid.local);
+
+    if (msg.to !== xmpp.jid.local) {
+        if (contacts.includes(msg.to)) {
+            console.log(`Message: ${body} sent to ${msg.to}`);
+            sendMessage(msg.to, body);
+        } else {
+            flooding(msg.to, body);
+        }
+    }
+  }
+});
+
+/**
+ * Algoritmo de flooding/ recibe un mensaje y si tenemos un destino, lo envía, si no, lo envía a todos los contactos
+ * @param {any} to
+ * @param {any} body
+ * @returns {any}
+ */
+const flooding = (to, body) => {
+  if (contacts.includes(to)) {
+      console.log(`Message: ${body} sent to ${to}`);
+      sendMessage(to, body);
+  } else {
+      contacts.forEach((contact) => {
+          console.log(`Message: ${body} sent to ${contact}`);
+          sendMessage(contact, body);
+      });
+  }
+  
+}
+
+/**
+ * Parsea un mensaje al formato JSON
+ * @param {any} msj
+ * @returns {any}
+ */
+const parseMessage = (msj) => {
+    const message = JSON.parse(msj);
+    return message;
+}
+
+/**
+ * Envia un mensaje a un destinatario
+ * @param {any} to
+ * @param {any} body
+ * @returns {any}
+ */
 async function sendMessage(to, body) {
   try {
     const stanza = xml('message', { to, type: 'chat' }, xml('body', {}, body));
     await xmpp.send(stanza);
-    console.log('Message sent');
+    console.log('Message sent✅');
   } catch (err) {
     console.error('❌ Message error:', err.toString());
   }
